f578820e753a60413309174b9126a1d8eb8c60c0
stephensund <47056935+stephensund@users.noreply.github.com>
2021-08-24 02:03:34 +0800

    update

---
+++ scripts/n1room.sh
@@ -0,0 +1,98 @@
+# Modify default IP
+sed -i 's/192.168.1.1/192.168.2.245/g' package/base-files/files/bin/config_generate
+
+# 调整CFLAGS等级为O3
+sed -i 's/Os/O3/g' include/target.mk
+
+# 默认开启 Irqbalance
+sed -i "s/enabled '0'/enabled '1'/g" feeds/packages/utils/irqbalance/files/irqbalance.config
+
+# 替换默认软件源为腾讯源（armv8）
+pushd files/etc/opkg
+touch distfeeds.conf
+cat > distfeeds.conf <<EOF
+src/gz core https://mirrors.cloud.tencent.com/lede/snapshots/targets/armvirt/64/packages
+src/gz base https://mirrors.cloud.tencent.com/lede/snapshots/packages/aarch64_cortex-a53/base
+src/gz luci https://mirrors.cloud.tencent.com/lede/snapshots/packages/aarch64_cortex-a53/luci
+src/gz packages https://mirrors.cloud.tencent.com/lede/snapshots/packages/aarch64_cortex-a53/packages
+src/gz routing https://mirrors.cloud.tencent.com/lede/snapshots/packages/aarch64_cortex-a53/routing
+src/gz telephony https://mirrors.cloud.tencent.com/lede/snapshots/packages/aarch64_cortex-a53/telephony
+EOF
+popd
+
+# Mod zzz-default-settings
+pushd package/lean/default-settings/files
+sed -i '/http/d' zzz-default-settings
+export orig_version="$(cat "zzz-default-settings" | grep DISTRIB_REVISION= | awk -F "'" '{print $2}')"
+sed -i "s/${orig_version}/${orig_version} ($(date +"%Y-%m-%d-%H%M"))/g" zzz-default-settings
+popd
+
+# Add luci-app-netspeedtest
+git clone https://github.com/sirpdboy/NetSpeedTest package/NetSpeedTest
+
+# Add Jerrykuku's packages
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/lua-maxminddb package/jerrykuku/lua-maxminddb
+git clone https://github.com/jerrykuku/luci-app-argon-config package/jerrykuku/luci-app-argon-config
+git clone https://github.com/jerrykuku/luci-app-vssr package/jerrykuku/luci-app-vssr
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
+sed -i 's/DEPENDS.*/& \+luci-theme-argon/g'  package/jerrykuku/luci-app-argon-config/Makefile
+
+# Add passwall
+rm -rf package/lean/dns2socks 
+rm -rf package/lean/ipt2socks 
+rm -rf package/lean/kcptun
+rm -rf feeds/packages/net/kcptun
+rm -rf package/lean/microsocks
+rm -rf package/lean/pdnsd-alt
+rm -rf package/lean/simple-obfs
+rm -rf package/lean/trojan
+rm -rf package/lean/v2ray-plugin
+git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
+# Add OpenClash
+git clone --depth=1 -b master https://github.com/vernesong/OpenClash package/openclash
+pushd package/openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
+
+
+# 补全Openclash依赖
+sed -i 's/DEPENDS.*/& \+kmod-tun +libcap-bin/g'  package/openclash/luci-app-openclash/Makefile
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+
+# Add adguardhome
+git clone https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
+
+# Add luci-theme-edge
+git clone -b 18.06 --depth=1 https://github.com/garypang13/luci-theme-edge package/luci-theme-edge
+
+
+# Add luci-app-diskman
+git clone --depth=1 https://github.com/SuLingGG/luci-app-diskman package/luci-app-diskman
+mkdir package/parted
+cp package/luci-app-diskman/Parted.Makefile package/parted/Makefile
+
+# Add latest smartdns package
+rm -rf feeds/packages/net/smartdns
+svn co https://github.com/Lienol/openwrt-packages/trunk/net/smartdns package/smartdns
+git clone -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
+
+# Add latest https-dns-proxy package
+rm -rf feeds/luci/applications/luci-app-https-dns-proxy
+rm -rf feeds/packages/net/https-dns-proxy
+svn co https://github.com/immortalwrt/luci/branches/openwrt-18.06-k5.4/applications/luci-app-https-dns-proxy feeds/luci/applications/luci-app-https-dns-proxy
+svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/net/https-dns-proxy feeds/packages/net/https-dns-proxy
+
+# preset cores for openclash
+mkdir -p files/etc/openclash/core
+open_clash_main_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+clash_tun_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+clash_game_url=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN | grep /clash-linux-armv8 | sed 's/.*url\": \"//g' | sed 's/\"//g')
+wget -qO- $open_clash_main_url | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $clash_tun_url | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $clash_game_url | tar xOvz > files/etc/openclash/core/clash_game
+chmod +x files/etc/openclash/core/clash*
+
